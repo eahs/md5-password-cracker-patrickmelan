a712c8a11e47b2c73edb4e9ae67655a2ef21f262
Patrick Melan <patrick.melan@outlook.com>
2023-10-25 11:20:15 -0400

    FINAL

--- PasswordCracker/PasswordCracker/Program.cs
+++ PasswordCracker/PasswordCracker/Program.cs
@@ -17,7 +17,7 @@ namespace PasswordCracker
     /// </summary>
     class Program
     {
-        public static string md5(string input)
+        public static string Md5(string input)
         {
             // Use input string to calculate MD5 hash
             using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
@@ -38,12 +38,39 @@ namespace PasswordCracker
         static void Main(string[] args)
         {
             string[] hashedPasswords = File.ReadAllLines("passwords_hashed.txt");
+            string[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            string[] cracked = new string[hashedPasswords.Length];
+
+            Dictionary<string, string> dict = new();
 
             Console.WriteLine("MD5 Password Cracker v1.0");
             
-            foreach (var pass in hashedPasswords)
+            for (int a = 0; a < 26; a++)
             {
-                Console.WriteLine(pass);
+                for (int b = 0; b < 26; b++)
+                {
+                    for (int c = 0; c < 26; c++)
+                    {
+                        for (int d = 0; d < 26; d++)
+                        {
+                            for (int e = 0; e < 26; e++)
+                            {
+                                // key: word, value: hashed word
+                                string word = alphabet[a] + alphabet[b] + alphabet[c] + alphabet[d] + alphabet[e];
+                                string hashed = Md5(word);
+                                dict.Add(hashed, word);
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < hashedPasswords.Length; i++)
+            {
+                if (dict.ContainsKey(hashedPasswords[i]))
+                {
+                    hashedPasswords[i] = dict[hashedPasswords[i]];
+                }
             }
 
             // Use this method to test if you managed to correctly crack all the passwords
